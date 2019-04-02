4884e8adfc96530ed627c35696251291e097bd69
AltenKylian <kylian.gehier@alten.com>
2019-04-02 15:17:09 +0200

    Abstraction Header-menu
    Passage du JSON object : modules dans le service créer précedement (navigationService)
    L'accès aux infos de module / menu / sous-menu passe désormais par le navigationService.
    TO-DO : la même chose côté Header-user

--- Ambroise-Web/src/app/header/header-menu/header-menu.component.ts
+++ Ambroise-Web/src/app/header/header-menu/header-menu.component.ts
@@ -2,135 +2,143 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderNavigation } from 'src/app/services/navigation.services';
 
 @Component({
-  selector: 'app-header-menu',
-  templateUrl: './header-menu.component.html',
-  styleUrls: ['./header-menu.component.scss']
+    selector: 'app-header-menu',
+    templateUrl: './header-menu.component.html',
+    styleUrls: ['./header-menu.component.scss']
 })
 export class HeaderMenuComponent implements OnInit {
 
-  currentModule = "Missions";
-  modules = {
-    "modules" : [
-        {
-            "label" : "Missions",
-            "menus" : [
-                {
-                    "label" : "Menu 1",
-                    "submenus" : [
-                        {
-                            "label" : "submenu1"
-                        },
-                        {
-                            "label" : "submenu2"
-                        }
-                    ]
-                },
-                {
-                    "label" : "Menu 2",
-                    "submenus" : [
-                        {
-                            "label" : "submenu1"
-                        },
-                        {
-                            "label" : "submenu2"
-                        }
-                    ]
-                },
-                {
-                    "label" : "Menu 3",
-                    "submenus" : [
-                        {
-                            "label" : "submenu1"
-                        },
-                        {
-                            "label" : "submenu2"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "label" : "Compétences",
-            "menus" : [
-                {
-                    "label" : "Menu 1",
-                    "submenus" : [
-                        {
-                            "label" : "submenu1"
-                        },
-                        {
-                            "label" : "submenu2"
-                        }
-                    ]
-                },
-                {
-                    "label" : "Menu 2",
-                    "submenus" : [
-                        {
-                            "label" : "submenu1"
-                        },
-                        {
-                            "label" : "submenu2"
-                        }
-                    ]
-                },
-                {
-                    "label" : "Menu 3",
-                    "submenus" : [
-                        {
-                            "label" : "submenu1"
-                        },
-                        {
-                            "label" : "submenu2"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "label" : "Forum",
-            "menus" : [
-                {
-                    "label" : "Menu 1",
-                    "submenus" : [
-                        {
-                            "label" : "submenu1"
-                        },
-                        {
-                            "label" : "submenu2"
-                        }
-                    ]
-                },
-                {
-                    "label" : "Menu 2",
-                    "submenus" : [
-                        {
-                            "label" : "submenu1"
-                        },
-                        {
-                            "label" : "submenu2"
-                        }
-                    ]
-                },
-                {
-                    "label" : "Menu 3",
-                    "submenus" : [
-                        {
-                            "label" : "submenu1"
-                        },
-                        {
-                            "label" : "submenu2"
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-};
+    /*currentModule = "Missions";
+    modules = {
+        "modules": [
+            {
+                "label": "Missions",
+                "menus": [
+                    {
+                        "label": "Menu 1",
+                        "submenus": [
+                            {
+                                "label": "submenu1"
+                            },
+                            {
+                                "label": "submenu2"
+                            }
+                        ]
+                    },
+                    {
+                        "label": "Menu 2",
+                        "submenus": [
+                            {
+                                "label": "submenu1"
+                            },
+                            {
+                                "label": "submenu2"
+                            }
+                        ]
+                    },
+                    {
+                        "label": "Menu 3",
+                        "submenus": [
+                            {
+                                "label": "submenu1"
+                            },
+                            {
+                                "label": "submenu2"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "label": "Compétences",
+                "menus": [
+                    {
+                        "label": "Menu 1",
+                        "submenus": [
+                            {
+                                "label": "submenu1"
+                            },
+                            {
+                                "label": "submenu2"
+                            }
+                        ]
+                    },
+                    {
+                        "label": "Menu 2",
+                        "submenus": [
+                            {
+                                "label": "submenu1"
+                            },
+                            {
+                                "label": "submenu2"
+                            }
+                        ]
+                    },
+                    {
+                        "label": "Menu 3",
+                        "submenus": [
+                            {
+                                "label": "submenu1"
+                            },
+                            {
+                                "label": "submenu2"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "label": "Forum",
+                "menus": [
+                    {
+                        "label": "Menu 1",
+                        "submenus": [
+                            {
+                                "label": "submenu1"
+                            },
+                            {
+                                "label": "submenu2"
+                            }
+                        ]
+                    },
+                    {
+                        "label": "Menu 2",
+                        "submenus": [
+                            {
+                                "label": "submenu1"
+                            },
+                            {
+                                "label": "submenu2"
+                            }
+                        ]
+                    },
+                    {
+                        "label": "Menu 3",
+                        "submenus": [
+                            {
+                                "label": "submenu1"
+                            },
+                            {
+                                "label": "submenu2"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }*/
 
-  constructor(private navigationService : HeaderNavigation) { }
+    modules : any
+    currentModule : String;
 
-  ngOnInit() {
-  }
+    constructor(private navigationService: HeaderNavigation) {
+        this.modules = this.navigationService.getModules();
+        this.currentModule = this.navigationService.getCurrentModul();
+        console.log(this.modules); //Working
+        console.log(this.currentModule); //Working
+    }
+
+    ngOnInit() {
+    }
 
 }
--- Ambroise-Web/src/app/services/navigation.services.ts
+++ Ambroise-Web/src/app/services/navigation.services.ts
@@ -5,9 +5,127 @@ import { Injectable } from '@angular/core';
 export class HeaderNavigation{
 
     private currentModul : String;
+    private modules = {
+        "modules" : [
+            {
+                "label" : "Missions",
+                "menus" : [
+                    {
+                        "label" : "Menu 1",
+                        "submenus" : [
+                            {
+                                "label" : "submenu1"
+                            },
+                            {
+                                "label" : "submenu2"
+                            }
+                        ]
+                    },
+                    {
+                        "label" : "Menu 2",
+                        "submenus" : [
+                            {
+                                "label" : "submenu1"
+                            },
+                            {
+                                "label" : "submenu2"
+                            }
+                        ]
+                    },
+                    {
+                        "label" : "Menu 3",
+                        "submenus" : [
+                            {
+                                "label" : "submenu1"
+                            },
+                            {
+                                "label" : "submenu2"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "label" : "Compétences",
+                "menus" : [
+                    {
+                        "label" : "Menu 1",
+                        "submenus" : [
+                            {
+                                "label" : "submenu1"
+                            },
+                            {
+                                "label" : "submenu2"
+                            }
+                        ]
+                    },
+                    {
+                        "label" : "Menu 2",
+                        "submenus" : [
+                            {
+                                "label" : "submenu1"
+                            },
+                            {
+                                "label" : "submenu2"
+                            }
+                        ]
+                    },
+                    {
+                        "label" : "Menu 3",
+                        "submenus" : [
+                            {
+                                "label" : "submenu1"
+                            },
+                            {
+                                "label" : "submenu2"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "label" : "Forum",
+                "menus" : [
+                    {
+                        "label" : "Menu 1",
+                        "submenus" : [
+                            {
+                                "label" : "submenu1"
+                            },
+                            {
+                                "label" : "submenu2"
+                            }
+                        ]
+                    },
+                    {
+                        "label" : "Menu 2",
+                        "submenus" : [
+                            {
+                                "label" : "submenu1"
+                            },
+                            {
+                                "label" : "submenu2"
+                            }
+                        ]
+                    },
+                    {
+                        "label" : "Menu 3",
+                        "submenus" : [
+                            {
+                                "label" : "submenu1"
+                            },
+                            {
+                                "label" : "submenu2"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    };
 
     constructor(){
-        this.currentModul = "Mission";
+        this.currentModul = "Missions";
     }
 
     setCurrrentModul(currentModul: String ){
@@ -16,6 +134,14 @@ export class HeaderNavigation{
 
     getCurrentModul(){
         return this.currentModul;
+    }
+
+    setModules(modules){
+        this.modules = modules;
+    }
+
+    getModules(){
+        return this.modules;
     }
 
 }
