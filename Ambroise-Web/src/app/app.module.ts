99d7b9aa0af02e9a953ea73a367cf0f55b2ca08e
Camille SCHNELL <camille.schnell@alten.com>
2019-04-02 13:01:01 +0200

    login and signup forms with validators

--- Ambroise-Web/src/app/app.module.ts
+++ Ambroise-Web/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -25,6 +25,7 @@ const appRoutes: Routes = [
       appRoutes
     ),
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
