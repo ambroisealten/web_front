cd13acd8be812440a43cf1f6556abb0803029671
AltenKylian <kylian.gehier@alten.com>
2019-04-03 15:24:37 +0200

    Test
    problème de branche donc je teste

--- Ambroise-Web/src/app/services/navigation.services.ts
+++ Ambroise-Web/src/app/services/navigation.services.ts
@@ -1,127 +1,129 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Injectable()
-export class HeaderNavigation{
+export class HeaderNavigation {
 
-    private currentModule : String;
+    private currentModule: String;
+    test: any;
     private modules = {
-        "modules" : [
+        "modules": [
             {
-                "label" : "Missions",
-                "roles" : ["admin", "cdr", "manager"],
-                "routerLink" : "mission",
-                "menus" : [
+                "label": "Missions",
+                "roles": ["admin", "cdr", "manager"],
+                "routerLink": "mission",
+                "menus": [
                     {
-                        "label" : "Menu Mission 1",
-                        "submenus" : [
+                        "label": "Menu Mission 1",
+                        "submenus": [
                             {
-                                "label" : "submenu1"
+                                "label": "submenu1"
                             },
                             {
-                                "label" : "submenu2"
+                                "label": "submenu2"
                             }
                         ]
                     },
                     {
-                        "label" : "Menu Mission 2",
-                        "submenus" : [
+                        "label": "Menu Mission 2",
+                        "submenus": [
                             {
-                                "label" : "submenu1"
+                                "label": "submenu1"
                             },
                             {
-                                "label" : "submenu2"
+                                "label": "submenu2"
                             }
                         ]
                     },
                     {
-                        "label" : "Menu Mission 3",
-                        "submenus" : [
+                        "label": "Menu Mission 3",
+                        "submenus": [
                             {
-                                "label" : "submenu1"
+                                "label": "submenu1"
                             },
                             {
-                                "label" : "submenu2"
+                                "label": "submenu2"
                             }
                         ]
                     }
                 ]
             },
             {
-                "label" : "Compétences",
-                "roles" : ["admin", "manager"],
-                "routerLink" : "competences",
-                "menus" : [
+                "label": "Compétences",
+                "roles": ["admin", "manager"],
+                "routerLink": "competences",
+                "menus": [
                     {
-                        "label" : "Menu Compétences 1",
-                        "submenus" : [
+                        "label": "Menu Compétences 1",
+                        "submenus": [
                             {
-                                "label" : "submenu1"
+                                "label": "submenu1"
                             },
                             {
-                                "label" : "submenu2"
+                                "label": "submenu2"
                             }
                         ]
                     },
                     {
-                        "label" : "Menu Compétences 2",
-                        "submenus" : [
+                        "label": "Menu Compétences 2",
+                        "submenus": [
                             {
-                                "label" : "submenu1"
+                                "label": "submenu1"
                             },
                             {
-                                "label" : "submenu2"
+                                "label": "submenu2"
                             }
                         ]
                     },
                     {
-                        "label" : "Menu Compétences 3",
-                        "submenus" : [
+                        "label": "Menu Compétences 3",
+                        "submenus": [
                             {
-                                "label" : "submenu1"
+                                "label": "submenu1"
                             },
                             {
-                                "label" : "submenu2"
+                                "label": "submenu2"
                             }
                         ]
                     }
                 ]
             },
             {
-                "label" : "Forum",
-                "roles" : ["admin", "cdr", "manager"],
-                "routerLink" : "forum",
-                "menus" : [
+                "label": "Forum",
+                "roles": ["admin", "cdr", "manager"],
+                "routerLink": "forum",
+                "menus": [
                     {
-                        "label" : "Menu Forum 1",
-                        "submenus" : [
+                        "label": "Menu Forum 1",
+                        "submenus": [
                             {
-                                "label" : "submenu1"
+                                "label": "submenu1"
                             },
                             {
-                                "label" : "submenu2"
+                                "label": "submenu2"
                             }
                         ]
                     },
                     {
-                        "label" : "Menu Forum 2",
-                        "submenus" : [
+                        "label": "Menu Forum 2",
+                        "submenus": [
                             {
-                                "label" : "submenu1"
+                                "label": "submenu1"
                             },
                             {
-                                "label" : "submenu2"
+                                "label": "submenu2"
                             }
                         ]
                     },
                     {
-                        "label" : "Menu Forum 3",
-                        "submenus" : [
+                        "label": "Menu Forum 3",
+                        "submenus": [
                             {
-                                "label" : "submenu1"
+                                "label": "submenu1"
                             },
                             {
-                                "label" : "submenu2"
+                                "label": "submenu2"
                             }
                         ]
                     }
@@ -130,25 +132,41 @@ export class HeaderNavigation{
         ]
     };
 
-    constructor(){
+    constructor(private httpClient: HttpClient) {
         this.currentModule = "Missions";
     }
 
-    setCurrentModule(currentModule: String ){
+    setCurrentModule(currentModule: String) {
         this.currentModule = currentModule;
     }
 
-    getCurrentModule(){
-        //console.log("A que coucou");
+    getCurrentModule() {
         return this.currentModule;
     }
 
-   /* setModules(modules){
-        this.modules = modules;
-    }*/
+    getModules() {
+        let token = window.sessionStorage.getItem("bearerToken");
 
-    getModules(){
-        return this.modules;
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': token != "" ? token : ''
+        });
+
+        let options = { headers: headers };
+
+        this.httpClient
+            .get('http://localhost:8080/configMenu')
+            .subscribe(data => {
+                console.log(JSON.stringify(data));
+                this.test = data;
+            }, error => {
+                console.log(error);// Error getting the data
+            });
+        setTimeout(() => {
+            console.log("coucou" + this.test);
+            this.modules = this.test;
+            return this.modules;
+        }, 50);
     }
 
 }
