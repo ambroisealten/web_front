f6b0dbe7a47dafeb845617f7dba5643a0848d947
Camille SCHNELL <camille.schnell@alten.com>
2019-04-05 11:45:10 +0200

    skills page tmp template

--- Ambroise-Web/src/app/content/skills-form/skills-form.component.ts
+++ Ambroise-Web/src/app/content/skills-form/skills-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-skills-form',
@@ -11,7 +12,22 @@ export class SkillsFormComponent implements OnInit {
     {manager: 'Joyce', date: '01/03/19', action: 'Création'},
     {manager: 'Joyce', date: '15/03/19', action: 'Mise à jour'}
   ];
-  displayedColumns: string[] = ['manager', 'date', 'action'];
+  lastModifDisplayedColumns: string[] = ['manager', 'date', 'action'];
+  lastModificationsArrayDS = new MatTableDataSource(this.lastModificationsArray);
+
+  skillsArray: any[] = [
+    {category: 'Langage', name: 'Java', grade: '3.5'},
+    {category: 'Langage', name: 'C++', grade: '3'}
+  ];
+  skillsDisplayedColumns: string[] = ['category', 'name', 'grade'];
+  skillsArrayDS = new MatTableDataSource(this.skillsArray);
+
+  softSkillsArray: any[] = [
+    {category: 'Gestion de projet', name: 'Cycle en V', grade: '3'},
+    {category: 'Gestion de projet', name: 'Scrum', grade: '3'}
+  ];
+  softSkillsDisplayedColumns: string[] = ['category', 'name', 'grade'];
+  softSkillsArrayDS = new MatTableDataSource(this.softSkillsArray);
 
   constructor() { }
 
@@ -20,6 +36,14 @@ export class SkillsFormComponent implements OnInit {
 
   onSubmitForm() {
     console.log("submit");
+  }
+
+  applyFilterSkills(filterValue: string) {
+    this.skillsArrayDS.filter = filterValue.trim().toLowerCase();
+  }
+
+  applyFilterSoftSkills(filterValue: string) {
+    this.softSkillsArrayDS.filter = filterValue.trim().toLowerCase();
   }
 
 }
---
+++ Ambroise-Web/src/assets/js/matrixScript.js
@@ -0,0 +1,56 @@
+window.onload = charged;
+window.onchange = charged;
+
+function charged(){
+        setTimeout(() => {
+            if (document.getElementsByClassName('list-item') != null || document.getElementsByClassName('list-item') != undefined){
+                let itemList = document.getElementsByClassName('list-item');
+                for(let item of itemList){
+                    item.addEventListener('click',test,false);
+                }
+            }
+            createMatrix("myChartCompetence", ['c++', 'Java', '',], [3, 3.5, 0]);
+            createMatrix("myChartSoftSkill", ['Scrum', 'Cycle en V', '',], [3, 3, 0]);
+        },50);
+}
+
+function test(event){
+    console.log("Bonjour");
+    let parentNode = event.target.parentNode;
+    while(parentNode.nodeName != "ONS-LIST-ITEM"){
+        parentNode = parentNode.parentNode;
+    }
+    let id = parentNode.id.substr(4,1);
+    document.getElementById('appTabbar').setActiveTab(id);
+    document.getElementById('menu').close();
+}
+
+function createMatrix(chartId, labels, data){
+    var ctx = document.getElementById(chartId).getContext('2d');
+    var myChart = new Chart(ctx, {
+    type: 'radar',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Note',
+            data: data,
+            backgroundColor: [
+                'rgba(00, 139, 210, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(00, 139, 210, 1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+
+        },
+        responsive: true,
+        maintainAspectRatio: false
+    }
+});
+}
