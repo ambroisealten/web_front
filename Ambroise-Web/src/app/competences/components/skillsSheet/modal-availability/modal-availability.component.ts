f5b1ae317bf7fa05a443aec3a638840a3f3d092f
Camille SCHNELL <camille.schnell@alten.com>
2019-05-17 16:30:19 +0200

    finished availability (without final design)

--- Ambroise-Web/src/app/competences/components/skillsSheet/modal-availability/modal-availability.component.ts
+++ Ambroise-Web/src/app/competences/components/skillsSheet/modal-availability/modal-availability.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DateAdapter, MatDialogRef } from '@angular/material';
-import { LoggerService, LogLevel } from 'src/app/services/logger.service';
-import { DurationType, OnDateAvailability, OnTimeAvailibility } from 'src/app/competences/models/person';
+import { DurationType, OnDateAvailability, OnTimeAvailability } from 'src/app/competences/models/person';
 
 @Component({
   selector: 'app-modal-availability',
@@ -13,7 +12,7 @@ export class ModalAvailabilityComponent implements OnInit {
   initDate: number;
   finalDate: number;
   duration: number;
-  durationType: DurationType;
+  durationType: string;
 
   constructor(private dialogRef: MatDialogRef<ModalAvailabilityComponent>,
     private adapter: DateAdapter<any>) { }
@@ -22,33 +21,30 @@ export class ModalAvailabilityComponent implements OnInit {
     this.adapter.setLocale('fr');
   }
 
-
-  /**
-   * On click on cancel button : close dialog with value 'canceled'
-   */
-  cancel() {
-    this.dialogRef.close('canceled');
-  }
-
   save() {
-    if(this.initDate != undefined && this.finalDate != undefined) {
+    if(this.initDate != undefined) {
+      // create object OnDateAvailability
+      let onDateAvailability = new OnDateAvailability();
+
       // convert dates to milliseconds
       let init = new Date(this.initDate).getTime();
-      let final = new Date(this.finalDate).getTime();
-      // create and init object OnDateAvailability
-      let onDateAvailability = new OnDateAvailability();
-      onDateAvailability.finalDate = final;
+      if(this.finalDate != undefined) {
+        let final = new Date(this.finalDate).getTime();
+        onDateAvailability.finalDate = final;
+      }
+
       onDateAvailability.initDate = init;
       // close with new object
       this.dialogRef.close(onDateAvailability);
     }
     else if(this.duration != undefined && this.durationType != undefined) {
       // create and init object OnTimeAvailability
-      let onTimeAvailibility = new OnTimeAvailibility();
-      onTimeAvailibility.duration = this.duration;
-      onTimeAvailibility.durationType = this.durationType;
+      let onTimeAvailability = new OnTimeAvailability();
+      onTimeAvailability.initDate = new Date().getTime();
+      onTimeAvailability.duration = this.duration;
+      onTimeAvailability.durationType = this.durationType;
       // close with new object
-      this.dialogRef.close(onTimeAvailibility);
+      this.dialogRef.close(onTimeAvailability);
     }
 
   }
