45c3193c9984bb124d7720bb49cc9d30a30b1efe
Andy CHABALIER <andy.chabalier@alten.com>
2019-05-20 14:38:54 +0200

    Some changes, cleaning code and correct update skill chart on changes

--- Ambroise-Web/src/app/competences/components/skillsSheet/array-skills/array-skills.component.ts
+++ Ambroise-Web/src/app/competences/components/skillsSheet/array-skills/array-skills.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, OnDestroy } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
 import { Skill, SkillGraduated } from 'src/app/competences/models/skillsSheet';
 import { SkillsListService } from '../../../services/skillsList.service';
@@ -14,7 +14,7 @@ import { FormControl } from '@angular/forms';
 /**
 * Component containing an array with skills or soft skills and their grades
 */
-export class ArraySkillsComponent implements OnInit {
+export class ArraySkillsComponent implements OnInit, OnDestroy {
 
   @Input() displayedColumns: string[]; // names of columns to display
   @Input() dataSourceArray: any[]; // data array
@@ -22,18 +22,18 @@ export class ArraySkillsComponent implements OnInit {
   @Input() searchBarHidden: boolean; // is search bar hidden
   @Input() dataSource: MatTableDataSource<SkillGraduated[]>; // data as MatTableDataSource
 
-  @Output() skillsEvent = new EventEmitter<SkillGraduated[]>() ;
+  @Output() skillsEvent = new EventEmitter<SkillGraduated[]>();
 
-  //Tableau contenant toutes les options (compétences) pour l'auto-complétion
-  options : string[];
-  filteredOptions : Observable<string[]>;
+  // Tableau contenant toutes les options (compétences) pour l'auto-complétion
+  options: string[];
+  filteredOptions: Observable<string[]>;
   myControl = new FormControl();
 
-  //Subscription ;
-  skillsSubscription ;
+  // Subscription ;
+  skillsSubscription;
 
-    
-  constructor(private skillsListService : SkillsListService) {
+
+  constructor(private skillsListService: SkillsListService) {
   }
 
   /**
@@ -46,7 +46,7 @@ export class ArraySkillsComponent implements OnInit {
     );
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
   }
 
   /**
@@ -54,43 +54,34 @@ export class ArraySkillsComponent implements OnInit {
    * @param  $event input grade
    */
   setToOneIfEmptyOrInvalid($event) {
-    let grade: string = $event.target.value;
-    let pattern: string = "^([1-3]([\\.|,]5)?)$|^4$"; // number between 1 and 4 (step 0,5) or 0
-    if(!grade.match(pattern) || $event.target.value == '') {
+    const grade: string = $event.target.value;
+    const pattern = '^([1-3]([\\.|,]5)?)$|^4$'; // number between 1 and 4 (step 0,5) or 0
+    if (!grade.match(pattern) || $event.target.value === '') {
       $event.target.value = 1;
     }
   }
 
   /**
-  * Filters array on input
-  * @param  filterValue input string
-  */
-  /*applyFilterSkills(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }*/
-
-    /**
-   * Cherche toutes les compétences en base
-   * @author Lucas Royackkers
-   */
-  getSkillsList(){
-    this.skillsListService.getAllTechSkills().subscribe(skillsList=> {
+ * Cherche toutes les compétences en base
+ * @author Lucas Royackkers
+ */
+  getSkillsList() {
+    this.skillsListService.getAllTechSkills().subscribe(skillsList => {
       this.options = (skillsList as Skill[]).map(skill => skill.name);
     });
   }
 
-    /**
-   * Filtre toutes les options qui correspondent à l'input user
-   * 
-   * @param value la valeur renseignée par l'utilisateur
-   * @author Lucas Royackkers
-   */
+  /**
+ * Filtre toutes les options qui correspondent à l'input user
+ * 
+ * @param value la valeur renseignée par l'utilisateur
+ * @author Lucas Royackkers
+ */
   private _filter(value: string): string[] {
-    if(value.length != 0){
+    if (value.length !== 0) {
       const filterValue = value.toLowerCase();
       return this.options.filter(option => option.toLowerCase().includes(filterValue));
-    }
-    else{
+    } else {
       return [];
     }
   }
@@ -100,16 +91,16 @@ export class ArraySkillsComponent implements OnInit {
   * @param  event skillName from input
   */
   addSkill(event) {
-    if(event.target.value.trim() != '') {
-      let skillName = event.target.value;
+    if (event.target.value.trim() !== '') {
+      const skillName = event.target.value;
 
       // if skillname not already in array : add it
-      if(this.dataSourceArray.findIndex(skillGraduated => skillGraduated.skill.name.toLowerCase().trim() === skillName.toLowerCase().trim()) == -1) {
+      if (this.dataSourceArray.findIndex(skillGraduated => skillGraduated.skill.name.toLowerCase().trim() === skillName.toLowerCase().trim()) === -1) {
 
-      this.dataSourceArray.push(new SkillGraduated(new Skill(skillName), 1));
-      this.dataSource = new MatTableDataSource(this.dataSourceArray);
+        this.dataSourceArray.push(new SkillGraduated(new Skill(skillName), 1));
+        this.dataSource = new MatTableDataSource(this.dataSourceArray);
 
-      this.updateDataSourceInService();
+        this.updateDataSourceInService();
       }
       event.target.value = '';
     }
@@ -120,14 +111,13 @@ export class ArraySkillsComponent implements OnInit {
   * @param  event button clicked in skill row
   */
   removeSkill(event) {
-    let skillName = event.target.closest('tr').childNodes[1].innerText; // get skillName from row
-    let skillIndex = this.dataSourceArray.findIndex(skillGraduated => skillGraduated.skill.name === skillName);
+    const skillName = event.target.closest('tr').childNodes[1].innerText; // get skillName from row
+    const skillIndex = this.dataSourceArray.findIndex(skillGraduated => skillGraduated.skill.name === skillName);
 
     this.dataSourceArray.splice(skillIndex, 1);
     this.dataSource = new MatTableDataSource(this.dataSourceArray);
 
     this.updateDataSourceInService();
-
   }
 
   /**
@@ -135,30 +125,29 @@ export class ArraySkillsComponent implements OnInit {
   * @param  event grade value from input
   */
   updateGradeEvent(event) {
-    let skillName = event.target.closest('tr').childNodes[1].innerText; // get skillName from same row as modified grade
-    let grade = event.target.parentElement.childNodes[1].value;
+    const skillName = event.target.closest('tr').childNodes[1].innerText; // get skillName from same row as modified grade
+    const grade = event.target.parentElement.childNodes[1].value;
 
-    this.dataSourceArray.forEach(function(skillGraduated) {
-      if(skillGraduated.skill.name == skillName){
-        if(grade == 1.5 && event.target.className == "incrementButton" ){
-          skillGraduated.grade = 2
-        } else if (grade == 1.5 && event.target.className == "decrementButton") {
-          skillGraduated.grade = 1
+    this.dataSourceArray.forEach(skillGraduated => {
+      if (skillGraduated.skill.name === skillName) {
+        if (grade === 1.5 && event.target.className === 'incrementButton') {
+          skillGraduated.grade = 2;
+        } else if (grade === 1.5 && event.target.className === 'decrementButton') {
+          skillGraduated.grade = 1;
         } else {
           skillGraduated.grade = +grade;
         }
       }
     });
-
     this.updateDataSourceInService();
   }
 
   /**
-  * Updates skills or softSkills array in skills service
-  */
+    * Updates skills or softSkills array in skills service
+    */
   updateDataSourceInService() {
     this.checkGradeValues();
-    this.skillsEvent.emit(this.dataSourceArray) ;
+    this.skillsEvent.emit(this.dataSourceArray);
   }
 
   /**
@@ -166,10 +155,10 @@ export class ArraySkillsComponent implements OnInit {
    * If grade invalid, set to 1
    */
   checkGradeValues() {
-    let pattern: string = "^([1-3]([\\.|,]5)?)$|^4$"; // number between 1 and 4 (step 0,5) or 0
+    const pattern = '^([1-3]([\\.|,]5)?)$|^4$'; // number between 1 and 4 (step 0,5) or 0
 
-    this.dataSourceArray.forEach(function(skillGraduated) {
-      if(!skillGraduated.grade.toString().match(pattern)) {
+    this.dataSourceArray.forEach(skillGraduated => {
+      if (!skillGraduated.grade.toString().match(pattern)) {
         skillGraduated.grade = 1;
       }
     });
