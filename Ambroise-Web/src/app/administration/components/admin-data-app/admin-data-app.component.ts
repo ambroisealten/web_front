6a82c321df2828d1045926183dbcb40863043b06
mmaquinghen <maxime.maquinghen@alten.com>
2019-06-05 16:11:33 +0200

    hot fix

--- Ambroise-Web/src/app/administration/components/admin-data-app/admin-data-app.component.ts
+++ Ambroise-Web/src/app/administration/components/admin-data-app/admin-data-app.component.ts
@@ -22,9 +22,19 @@ import { UserRole } from 'src/app/administration/models/User' ;
   styleUrls: ['./admin-data-app.component.scss']
 })
 export class AdminDataAppComponent implements OnInit, OnDestroy {
-
-  //Subscriptions
   submenusSubscription;
+
+  // SoftSkill data 
+  softSkills: SoftSkill[];
+  softSkillsSources: MatTableDataSource<any[]> = new MatTableDataSource();
+  displayedsoftSkillsColumns: string[] = ['Nom', 'Delete'];
+
+  // agency data 
+  agencies: Agency[];
+  currentAgency: string[];
+  agenciesSources: MatTableDataSource<any[]> = new MatTableDataSource();
+  displayedAgencyColumns: string[] = ['Nom', 'Lieu', 'Type de lieu', 'Delete'];
+
 
   // User data 
   users: User[];
@@ -42,6 +52,10 @@ export class AdminDataAppComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.createMenu();
     this.submenusSubscription = this.subMenusService.menuActionObservable.subscribe(action => this.doAction(action));
+    this.fetchSoftSkills();
+    this.fetchAgencies();
+    this.fetchUsers();
+    console.log(this.users);
   }
 
   ngOnDestroy() {
@@ -50,6 +64,20 @@ export class AdminDataAppComponent implements OnInit, OnDestroy {
     } else {
       LoggerService.log('ERROR : SubMenusSubscription (Admin Data App) should have been set', LogLevel.DEV);
     }
+  }
+
+  getAgencies() {
+    return this.agencies;
+  }
+
+  fetchAgencies() {
+    this.agencies = [];
+    this.adminService.makeRequest('/agencies', 'get', '').subscribe((agenciesList: Agency[]) => {
+      for (const agency of agenciesList) {
+        this.agencies.push(new Agency(agency.name, agency.place, agency.placeType));
+      }
+      this.agenciesSources = new MatTableDataSource<any>(this.agencies)
+    });
   }
 
   getUser() {
@@ -66,6 +94,57 @@ export class AdminDataAppComponent implements OnInit, OnDestroy {
     });
   }
 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      this.saveSoftSkillsOrder(event.container.data);
+    }
+  }
+
+
+  saveSoftSkillsOrder(dropList: any[]) {
+    const dialogProgress = ProgressSpinnerComponent.openDialogProgress(this.dialog);
+    const finalList = [];
+    let order = 0;
+    for (let i = 0; i < dropList.length; i++) {
+      order++;
+      const child = dropList[i];
+      child.setOrder(order);
+      finalList.push(child);
+    }
+    if (finalList.length === 0) {
+      const ok = confirm('Êtes-vous sûr de ne vouloir mettre aucune compétence Soft dans la base de données ?');
+      if (!ok) {
+        return;
+      }
+    }
+    const postParams = {
+      softSkillsList: finalList
+    }
+    this.adminService.makeRequest('/softSkillsOrder', 'put', postParams).subscribe((responses: any[]) => {
+      this.fetchSoftSkills();
+      dialogProgress.close();
+      this.errorService.handleResponses(responses, 200);
+    });
+  }
+
+  getSoftSkills() {
+    return this.softSkills;
+  }
+
+  fetchSoftSkills() {
+    this.softSkills = [];
+    this.adminService.makeRequest('/softskills', 'get', '').subscribe((softSkillList: SoftSkill[]) => {
+      for (const softSkill of softSkillList) {
+        this.softSkills.push(new SoftSkill(softSkill.name, softSkill.order));
+      }
+      this.softSkills.sort((e1, e2) => e1.getOrder() - e2.getOrder());
+      this.softSkillsSources = new MatTableDataSource<any>(this.softSkills);
+      window.sessionStorage.setItem('softSkills', JSON.stringify(this.softSkills));
+    });
+
+  }
+
   onClickSynchroniseGeoApi() {
     const dialogProgress = ProgressSpinnerComponent.openDialogProgress(this.dialog);
     this.adminService.makeRequest('/admin/synchronize/geographics', 'post', '').subscribe((response) => {
@@ -73,6 +152,198 @@ export class AdminDataAppComponent implements OnInit, OnDestroy {
       this.errorService.handleResponse(response);
     });
     // TODO Attention, on a pas de moyen de savoir si une mise à jour à été faite sur la base distante
+  }
+
+  removeAgency(agency: Agency) {
+    const dialogProgress = ProgressSpinnerComponent.openDialogProgress(this.dialog);
+    const postParams = {
+      name: agency.getName(),
+      place: agency.getPlace(),
+      placeType: agency.getPlaceType()
+    };
+    this.adminService.makeRequest('/agency', 'delete', postParams).subscribe((response) => {
+      this.fetchAgencies();
+      dialogProgress.close();
+      this.errorService.handleResponse(response);
+    });
+  }
+
+  removeSoftSkill(softSkill: SoftSkill) {
+    const dialogProgress = ProgressSpinnerComponent.openDialogProgress(this.dialog);
+    const postParams = {
+      name: softSkill.getName(),
+    };
+    this.adminService.makeRequest('/skill', 'delete', postParams).subscribe((response) => {
+      this.fetchSoftSkills();
+      dialogProgress.close();
+      this.errorService.handleResponse(response);
+    });
+  }
+
+  addAgency() {
+    const agency = new Agency('', '', '');
+    const dialogAgency = this.openDialogAgency(agency);
+
+    dialogAgency.afterClosed().subscribe(
+      (data: any) => {
+        if (data) {
+          const dialogProgress = ProgressSpinnerComponent.openDialogProgress(this.dialog);
+          agency.setName(data.name);
+          agency.setPlace(data.place);
+          agency.setPlaceType(data.placeType);
+          const postParams = {
+            name: agency.getName(),
+            place: agency.getPlace(),
+            placeType: agency.getPlaceType()
+          };
+          this.adminService.makeRequest('/agency', 'post', postParams).subscribe((response) => {
+            this.fetchAgencies();
+            dialogProgress.close();
+            this.errorService.handleResponse(response);
+          });
+        }
+      });
+  }
+
+  deleteAgency(agencyName, agencyPlace, agencyPlaceType) {
+    //TODO
+
+  }
+  changeAgency(agencyName, agencyPlace, agencyPlaceType) {
+
+    const agency = new Agency(agencyName, agencyPlace, agencyPlaceType);
+    const dialogAgency = this.openDialogAgency(agency);
+
+    dialogAgency.afterClosed().subscribe(
+      (data: any) => {
+        if (data) {
+          const dialogProgress = ProgressSpinnerComponent.openDialogProgress(this.dialog);
+          agency.setName(data.name);
+          agency.setPlace(data.place);
+          agency.setPlaceType(data.placeType);
+          const postParams = {
+            name: agency.getName(),
+            place: agency.getPlace(),
+            placeType: agency.getPlaceType()
+          };
+          this.adminService.makeRequest('/agency', 'post', postParams).subscribe((response) => {
+            this.fetchAgencies();
+            dialogProgress.close();
+            this.errorService.handleResponse(response);
+          });
+        }
+      });
+  }
+
+  addSoftSkill() {
+    const softSkill = new SoftSkill('', this.softSkills.length + 1);
+    const dialogSoftSkill = this.openDialogSoftSkill(softSkill);
+
+    dialogSoftSkill.afterClosed().subscribe(
+      (data: any) => {
+        if (data) {
+          const dialogProgress = ProgressSpinnerComponent.openDialogProgress(this.dialog);
+          softSkill.setName(data.name);
+          const postParams = {
+            name: softSkill.name,
+            isSoft: softSkill.isSoft,
+            order: softSkill.order
+          };
+          this.adminService.makeRequest('/skill', 'post', postParams).subscribe((response) => {
+            this.fetchSoftSkills();
+            dialogProgress.close();
+            this.errorService.handleResponse(response);
+          });
+        }
+      });
+  }
+
+  updateAgency(agency: Agency) {
+    const dialogAgency = this.openDialogAgency(agency);
+
+    dialogAgency.afterClosed().subscribe(
+      (data: any) => {
+        if (data) {
+          const dialogProgress = ProgressSpinnerComponent.openDialogProgress(this.dialog);
+          const previousName = agency.getName();
+          agency.setName(data.name);
+          agency.setPlace(data.place);
+          agency.setPlaceType(data.placeType);
+          const postParams = {
+            oldName: previousName,
+            name: agency.getName(),
+            place: agency.getPlace(),
+            placeType: agency.getPlaceType()
+          };
+          this.adminService.makeRequest('/agency', 'put', postParams).subscribe((response) => {
+            this.fetchAgencies();
+            dialogProgress.close();
+            this.errorService.handleResponse(response);
+          });
+        }
+      });
+  }
+
+  updateSoftSkill(softSkill: SoftSkill) {
+    const dialogSoftSkill = this.openDialogSoftSkill(softSkill);
+
+    dialogSoftSkill.afterClosed().subscribe(
+      (data: any) => {
+        if (data) {
+          const dialogProgress = ProgressSpinnerComponent.openDialogProgress(this.dialog);
+          const previousName = softSkill.getName();
+          softSkill.setName(data.name);
+          const postParams = {
+            oldName: previousName,
+            name: softSkill.getName(),
+            isSoft: softSkill.getIsSoft(),
+          };
+          this.adminService.makeRequest('/skill', 'put', postParams).subscribe((response) => {
+            this.fetchSoftSkills();
+            dialogProgress.close();
+            this.errorService.handleResponse(response);
+          });
+        }
+      });
+  }
+
+  openDialogAgency(agency: Agency) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.hasBackdrop = true;
+    dialogConfig.direction = 'ltr';
+    dialogConfig.closeOnNavigation = true;
+
+    dialogConfig.data = {
+      id: 1,
+      title: 'Agency',
+      description: 'Agence',
+      name: agency.getName(),
+      place: agency.getPlace()
+    };
+
+    return this.dialog.open(DataAgencyDialogComponent, dialogConfig);
+  }
+
+  openDialogSoftSkill(softSkill: SoftSkill) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.hasBackdrop = true;
+    dialogConfig.direction = 'ltr';
+    dialogConfig.closeOnNavigation = true;
+
+    dialogConfig.data = {
+      id: 1,
+      title: 'Soft Skill',
+      description: 'Soft Skill',
+      name: softSkill.getName(),
+    };
+
+    return this.dialog.open(DataSoftSkillDialogComponent, dialogConfig);
   }
 
   doAction(action: string) {
@@ -140,30 +411,7 @@ export class AdminDataAppComponent implements OnInit, OnDestroy {
     return this.dialog.open(DataUserManagementDialogComponent, dialogConfig);
   }
 
-  changeUser(userName, userForName, userMail, userRole) {
-    const user = new User(userName, userForName, userMail, userRole);
-    const dialogUser = this.openDialogModifUser(user);
-
-    dialogUser.afterClosed().subscribe(
-      (data: any) => {
-        if (data) {
-          const dialogProgress = ProgressSpinnerComponent.openDialogProgress(this.dialog);
-          agency.setName(data.name);
-          agency.setPlace(data.place);
-          agency.setPlaceType(data.placeType);
-          const postParams = {
-            name: agency.getName(),
-            place: agency.getPlace(),
-            placeType: agency.getPlaceType()
-          };
-          this.adminService.makeRequest('/agency', 'post', postParams).subscribe((response) => {
-            this.fetchAgencies();
-            dialogProgress.close();
-            this.errorService.handleResponse(response);
-          });
-        }
-      });
-  }
+  
 
   openDialogModifUser(user: User) {
     const dialogConfig = new MatDialogConfig();
