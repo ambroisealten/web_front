8f47f7e08b317d351808ace34b1a383d32e1beb5
Andy CHABALIER <andy.chabalier@alten.com>
2019-06-11 18:00:42 +0200

    change headers to store cookies

--- Ambroise-Web/src/app/login/services/token.service.ts
+++ Ambroise-Web/src/app/login/services/token.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import * as sha512 from 'js-sha512';
@@ -43,16 +43,19 @@ export class TokenService {
             + ':::' + postParams.pswd, LogLevel.DEBUG);
 
         // Requête POST au WS : login => Objectif récupérer un token de session valide
-        this.httpClient.post(environment.serverAddress + '/login', postParams)
+        this.httpClient.post(environment.serverAddress + '/login', postParams, {
+            withCredentials: true,
+            observe: 'response' as 'response'
+        })
             // Timeout pour éviter de rester bloquer sur l'authentification si serveur injoignable
             .pipe(catchError(err => this.errorService.handleError(err)))
             // Effectue une action dès la réception du token
-            .subscribe(response => {
+            .subscribe((response: HttpResponse<any>) => {
                 // Check si la propriété Token existe
-                if (response.hasOwnProperty('access') && response.hasOwnProperty('refresh')) {
+                if (response.body.hasOwnProperty('access') && response.body.hasOwnProperty('refresh')) {
                     // On store le token dans le sessionStorage du navigateur
-                    window.sessionStorage.setItem('bearerToken', response['access']['token']);
-                    window.sessionStorage.setItem('refreshToken', response['refresh']['token']);
+                    window.sessionStorage.setItem('bearerToken', response.body['access']['token']);
+                    window.sessionStorage.setItem('refreshToken', response.body['refresh']['token']);
                     // Notification de l'observable pour notifier la réception d'un token
                     this.tokenReceptionState.next(true);
                 } else {
